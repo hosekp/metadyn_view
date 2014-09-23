44611c3b3669d98e28be2dba2820bb062dfafa00
Petr Hošek <ringael@seznam.cz>
2014-09-23 14:42:41 +0200

    Exporter working
    Exporter zprovozněn do funkční, ale ne moc vzhledné podoby. Funguje přes
    preserveDrawingBuffer, což trochu zpomaluje vykreslování. V budoucnu se
    možná použije readPixels, který bude šetřit výkon, když nebude potřeba

--- public_html/js/view/exporter.js
+++ public_html/js/view/exporter.js
@@ -25,13 +25,37 @@ $.extend(view.exporter,{
         if(!this.inited){this.init();}
         this.$cont.show();
         this.ctx=this.$canvas[0].getContext("2d");
+        var $main_cont=$("#main_cont");
+        this.resize($main_cont.width(),$main_cont.height());
+        this.redraw();
         $("#cont").hide();
-        control.control.kill();
+    },
+    resize:function(width,height){
+        this.$canvas.width(width).height(height).attr({width:width,height:height});
     },
     close:function(){
         this.$cont.hide();
         $("#cont").show();
-        control.control.revive();
         //control.settings.png.set(false);
-    }
+    },
+    redraw:function(){
+        var ctx=this.ctx;
+        var $axi_y=$("#axi_y");
+        var $axi_x=$("#axi_x");
+        var $main_can=$("#main_can");
+        var ncv=control.settings.ncv.get();
+        ctx.drawImage($axi_y[0],0,0);
+        ctx.drawImage($axi_x[0],$axi_y.width(),$axi_y.height());
+        if(ncv>1){
+            var $axi_z=$("#axi_z");
+            ctx.drawImage($axi_z[0],$axi_y.width()+$axi_x.width(),($axi_y.height()-$axi_z.height())/2);
+            /*draw.drawer.drawer.getBuffer(function(buffer){
+                var imageData=ctx.getImageData(0,0,$main_can.width(),$main_can.height());
+                imageData.data.set(buffer);
+                ctx.putImageData(imageData,$axi_y.width(),0);
+            });*/
+        }else{
+        }
+        ctx.drawImage($main_can[0],$axi_y.width()+5,5);
+    },
 });
