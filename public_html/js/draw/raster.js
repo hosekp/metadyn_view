932c9aa26e430e06a7da4c6c5b380b4f62b30e18
Petr Hošek <ringael@seznam.cz>
2014-10-08 18:34:18 +0200

    Raster finished
    Zprovoznění Rasteru bylo až příliš jednoduché. Chybí ještě PBC a
    kontury, které implementovány asi vůbec nebudou

--- public_html/js/draw/raster.js
+++ public_html/js/draw/raster.js
@@ -10,40 +10,57 @@ $.extend(draw.raster,{
         }
         this.ctx=this.$can[0].getContext("2d");
         this.inited=true;
+        this.cscale=new Uint32Array(1000);
+        for(var i=0;i<1000;i++){
+            this.cscale[i]=this.colorScale(i/1000);
+        }
     },
     draw:function(array,zmax){
+        //this.profiler.init();
         var ctx=this.ctx;
-        //var resol=control.settings.resol.get();
-        array=[0,1,2,3,1,2,3,4,2,3,4,5,3,4,5,6];
-        zmax=6;
-        var resol=4;
+        var resol=control.settings.resol.get();
+        if(false){
+            var resol=10;
+            array=new Float32Array(resol*resol);
+            for(var j=0;j<resol;j++){
+                for(var i=0;i<resol;i++){
+                    array[j*resol+i]=j+i;
+                }
+            }
+            zmax=(resol-1)*2;
+        }
+        zmax=1;
         var resol1=resol-1;
         var gw=this.width/resol1;
         var gh=this.height/resol1;
-        var hwh=0;
-        var wh,ww,hww,hwh,iw,ih,h0w0,h0w1,h1w0,h1w1,sw,sh,inter,fr=0;
+        //this.profiler.time(1);
+        var wh,ww,hww,hwh=0,iw,ih,h0w0,h0w1,h1w0,h1w1,sw,sh,dinter,inter0,sumhw,fr=0,whxwi,wwl,whl;
         h0w1=array[fr]/zmax;
         h1w1=array[fr+resol]/zmax;
         for(sh=0;sh<resol;sh++){
             hww=0;
-            sw=0;
-            /*h0w0=array[sw+sh*resol]/zmax;
-            h1w0=array[sw+sh*resol+resol]/zmax;
-            h0w1=array[sw+1+sh*resol]/zmax;
-            h1w1=array[sw+1+sh*resol+resol]/zmax;*/
-            for(;sw<resol1;sw++){
+            for(sw=0;sw<resol1;sw++){
                 h0w0=h0w1;
                 h1w0=h1w1;
                 h0w1=array[fr+1]/zmax;
                 h1w1=array[fr+1+resol]/zmax;
                 //var h0w0=array[sh*resol+sw]/zmax;
-                for(wh=hwh;wh<(sh+1)*gh;wh++){
-                    for(ww=hww;ww<(sw+1)*gw;ww++){
+                sumhw=h0w0-h0w1-h1w0+h1w1;
+                whl=(sh+1)*gh;
+                for(wh=hwh;wh<whl;wh++){
+                    ih=wh/gh-sh;
+                    whxwi=wh*this.width;
+                    inter0=(h0w0*(1-ih)+h1w0*ih)*1000;
+                    //inter1=(h0w1*(1-ih)+h1w1*ih)*1000;
+                    //dinter=(h0w1*(1-ih)+h1w1*ih)*1000-inter0;
+                    dinter=(h0w1-h0w0+ih*sumhw)*1000;
+                    wwl=(sw+1)*gw;
+                    for(ww=hww;ww<wwl;ww++){
                         iw=ww/gw-sw;
-                        ih=wh/gh-sh;
-                        inter=this.interpolate(h0w0,h0w1,h1w0,h1w1,iw,ih);
                         //inter=h0w0*(1-iw)*(1-ih)+h0w1*iw*(1-ih)+h1w0*(1-iw)*ih+h1w1*iw*ih;
-                        this.work32[ww+wh*this.width]=this.colorScale(inter);
+                        //this.work32[ww+whxwi]=this.colorScaleWrap(inter);
+                        this.work32[ww+whxwi]=this.cscale[Math.floor(inter0+dinter*iw)];
+                        //this.work32[ww+whxwi]=this.cscale[Math.floor(inter0*(1-iw)+inter1*iw)];
                         //this.colorScale((ww*2+wh)/(this.width*2+this.height),ww+wh*this.width,this.workarr);
                     }
                 }
@@ -54,15 +71,19 @@ $.extend(draw.raster,{
             h0w1=array[fr]/zmax;
             h1w1=array[fr+resol]/zmax;
             hwh=wh;
-        } 
+        }
         /*for(var h=0;h<this.height;h++){
             for(var w=0;w<this.width;w++){
                 this.colorScale((w+h)/(this.width+this.height),w+h*this.width,this.workarr);
             }
         }*/
+        //this.profiler.time(2);
         this.imageData.data.set(this.work8);
+        //this.profiler.time(3);
         this.ctx.putImageData(this.imageData,0,0);
-        manage.console.debug("Raster draw");
+        //manage.console.debug("Raster draw");
+        //this.profiler.time(4);
+        //this.profiler.print();
     },
     resize:function(width,height){
         this.width=width;
@@ -81,9 +102,47 @@ $.extend(draw.raster,{
     colorScale:function(d){
         var sigma=1000.0;
         var hei = 380.0;
+        /*for(var i=0;i<100;i++){
+            var f=i*i;
+        }*/
         return (255 << 24) |
             (Math.min(Math.max(hei-Math.abs(d-0.77)*sigma,0.0),255.0) << 16) |
             (Math.min(Math.max(hei-Math.abs(d-0.49)*sigma,0.0),255.0) << 8) |
              Math.min(Math.max(hei-Math.abs(d-0.23)*sigma,0.0),255.0);
+    },
+    colorScaleWrap:function(d){
+        return this.cscale[Math.floor(d*1000)];
+    },
+    profiler:{
+        vals:[0,0,0,0,0],
+        nvals:[0,0,0,0,0],
+        lasttime:null,
+        init:function(){this.lasttime=window.performance.now();},
+        time:function(ind){
+            var now=window.performance.now();
+            //this.vals[ind]+=now-this.lasttime;
+            //this.nvals[ind]++;
+            this.vals[ind]+=now-this.lasttime;
+            this.lasttime=now;
+        },
+        reset:function(){
+            this.vals=[0,0,0,0,0];
+            this.nvals=[0,0,0,0,0];
+        },
+        print:function(){
+            /*manage.console.debug("\
+1: "+(this.vals[1]/this.nvals[1])+"\n\
+2: "+(this.vals[2]/this.nvals[2])+"\n\
+3: "+(this.vals[3]/this.nvals[3])+"\
+");
+        }*/
+            manage.console.debug("\
+1: "+(this.vals[1].toFixed(2))+"\n\
+2: "+(this.vals[2].toFixed(2))+"\n\
+3: "+(this.vals[3].toFixed(2))+"\n\
+4: "+(this.vals[4].toFixed(2))+"\
+");
+            this.reset();
+        }
     }
 });
