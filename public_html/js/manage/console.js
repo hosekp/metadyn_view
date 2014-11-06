3293d1f3a923ad3a6fb1a0924f79980bcd39700b
hosekp <ringael@seznam.cz>
2014-11-06 14:59:30 +0100

    Console "success" type
    Console nyní umí tvořit i zelené zprávy

--- public_html/js/manage/console.js
+++ public_html/js/manage/console.js
@@ -7,6 +7,7 @@ $.extend(manage.console,{
     /* 0= nic
      * 1= pouze errory
      * 2= i warningy
+     * 2.5 = i successy
      * 3= i logy
      * 4= i debug
      */
@@ -17,7 +18,7 @@ $.extend(manage.console,{
         if(control.settings.loglvl.get()<loglvl){return;}
         if(this.$console===null){this.init();}
         var txt=this.constext;
-        var colors={1:"red",2:"orange",3:"black",4:"blue"};
+        var colors={1:"red",2:"orange",3:"black",4:"blue",2.5:"green"};
         txt.push("<span style='color:"+colors[loglvl]+"'>"+string+"</span>");
         if(txt.length>20){
             txt.shift();
@@ -31,6 +32,7 @@ $.extend(manage.console,{
     debug:function(){this.addText(Lang.apply(null,arguments),4);},
     log:function(){this.addText(Lang.apply(null,arguments),3);},
     warning:function(){this.addText(Lang.apply(null,arguments),2);},
-    error:function(){this.addText(Lang.apply(null,arguments),1);}
+    error:function(){this.addText(Lang.apply(null,arguments),1);},
+    success:function(){this.addText(Lang.apply(null,arguments),2.5);}
 });
 
