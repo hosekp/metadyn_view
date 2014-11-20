070d142d1bd2b3f2428529e181fbbb1d5075124f
Petr Hošek <ringael@seznam.cz>
2014-11-20 15:50:12 +0100

    Licensing stated
    První zkoušky licencování a průchodu přes libre.js

--- public_html/js/compute/axi.js
+++ public_html/js/compute/axi.js
@@ -1,3 +1,6 @@
+/** @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3-or-Later
+* Copyright (C) 2014  Petr Hošek
+*/
 if(typeof compute==="undefined"){compute={};}
 if(typeof compute.axi==="undefined"){compute.axi={};}
 $.extend(compute.axi,{
@@ -218,3 +221,4 @@ $.extend(compute.axi,{
         }
     }*/
 });
+// @license-end
