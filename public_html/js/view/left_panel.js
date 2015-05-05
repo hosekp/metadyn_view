6dfae4e8ceffdd59fd2840698c667fe466678fd2
Petr Hošek <ringael@seznam.cz>
2015-05-05 13:55:23 +0200

    Visitor count - html version
    kvůli změně index.php na index.html bylo nutné předělat php zjišťování
    počtu návštěvníků na ajax verzi. Snad bude fungovat

--- public_html/js/view/left_panel.js
+++ public_html/js/view/left_panel.js
@@ -10,6 +10,7 @@ $.extend(view.panel,{
     },
     $left:null,
     needRender:true,
+    count:100,
     init:function(){
         control.control.everysec(this,"render");
         control.settings.lang.subscribe(this,"draw");
@@ -20,6 +21,7 @@ $.extend(view.panel,{
     },
     redraw:function(){
         this.needRender=true;
+        //this.getVisitors();  // for frequent update of visitors count
     },
     render:function(){
         var lang,template,obj,rendered,millis;
@@ -48,8 +50,31 @@ $.extend(view.panel,{
         
         this.needRender=false;
     },
+    checkin:function(){
+        this.getVisitors(true);
+    },
+    getVisitors:function(register){
+        $.ajax({
+            url:"../../metadyn_counter/main.php",
+            data:{register:register},
+            dataType:"text",
+            method:"GET",
+            success:function(data){
+                if(!isNaN(data)){
+                    view.panel.count=data;
+                    view.panel.needRender=true;
+                }else{
+                    var millis=new Date().getTime()-new Date(2014,12-1,11).getTime();
+                    view.panel.count=Math.round(millis/(24*60*60*1000)*12);
+                    view.panel.needRender=true;
+                }
+            },
+            error:function(data){}
+        });
+    },
     notify:function(args){
         if(args==="render"){this.render();}
         if(args==="draw"){this.redraw();}
     }
 });
+view.panel.checkin();
