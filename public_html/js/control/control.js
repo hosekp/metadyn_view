08960275ff41b0cd3c6f648d745e8438fdbbff21
Petr Hošek <ringael@seznam.cz>
2017-01-25 15:31:19 +0100

    format

--- public_html/js/control/control.js
+++ public_html/js/control/control.js
@@ -1,170 +1,188 @@
 /** @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3-or-Later
-* Copyright (C) 2014  Petr Hošek
-*/
-if(window.control===undefined){var control={};}
-if(control.control===undefined){control.control={};}
-$.extend(control.control,{
-    actratio:0,
-    wantedratio:0,
-    stats:null,
-    needRedraw:true,
-    running:false,
-    lasttime:0,
-    fastObser:[],
-    slowObser:[],
-    RAFprefix:null,
-    lastSlow:1000000,
-    init:function(){
-        var stt=new Stats(),
-        sett=control.settings;
-        stt.setMode(0);
-        $("#all").append(stt.domElement);
-        this.stats=stt;
-        this.findRAF();
-        this.cycle(this.lasttime);
-        sett.zoom.subscribe(this,null);
-        sett.frameposx.subscribe(this,null);
-        sett.frameposy.subscribe(this,null);
-        sett.resol.subscribe(this,null);
-        sett.play.subscribe(this,"toggle");
-        view.axi.subscribe(this,"resize");
-    },
-    start:function(){
-        if(this.actratio>=1){this.reset();}
-        this.running=true;
-    },
-    cycle:function(stamp){
-        var dt,nratio,is,lis,rat;
-        if(stamp === undefined){
-            stamp=new Date().getTime();
-        }
-        dt = stamp - this.lasttime;
-        if(this.running){
-            nratio=this.wantedratio+dt*control.settings.speed.get()/10000;
-            if(nratio>1){
-                nratio=1;
-            }
-            if(this.actratio===1){
-                if(control.settings.loop.get()){
-                    nratio=this.reset();
-                }else{
-                    control.settings.play.set(false);
-                }
-            }
-            this.setWanted(nratio);
-        }
-        //manage.console.debug("Control:","cycle");
-        this.stats.begin();
-        for(is=0;is<this.fastObser.length;is+=1){
-            lis=this.fastObser[is];
-            lis.ctx.notify(lis.call);
-        }
-        if(this.lastSlow>100){
-            for(is=0;is<this.slowObser.length;is+=1){
-                lis=this.slowObser[is];
-                lis.ctx.notify(lis.call);
-            }
-            this.lastSlow=0;
-        }
-        this.lastSlow+=dt;
-        rat=this.draw();
-        this.set(rat);
-        this.stats.end();
-        this.lasttime = stamp;
-        window.requestAnimationFrame(function(stamp){
-            control.control.cycle(stamp);
-        });
-    },
-    everytick:function(obj,func){
-        this.fastObser.push({ctx:obj,call:func});
-    },
-    everysec:function(obj,func){
-        this.slowObser.push({ctx:obj,call:func});
-    },
-    unsubscribe:function(obj,func){
-        var i,lis;
-        for(i=0;i<this.fastObser.length;i+=1){
-            lis=this.fastObser[i];
-            if(lis.ctx===obj&&lis.call===func){
-                this.fastObser.pop(i);
-                return;
-            }
-        }
-        for(i=0;i<this.slowObser.length;i+=1){
-            lis=this.slowObser[i];
-            if(lis.ctx===obj&&lis.call===func){
-                this.slowObser.pop(i);
-                return;
-            }
-        }
-        manage.console.warning("Control:","Unsubscription of",func,"failed");
-    },
-    findRAF:function(){
-        var vendors,i,vRAF;
-        vendors = ['ms', 'moz', 'webkit', 'o'];
-        if(!window.requestAnimationFrame){
-            for(i = 0; i < vendors.length;i+=1) {
-                vRAF=window[vendors[i]+'RequestAnimationFrame'];
-                if(vRAF){
-                    window.requestAnimationFrame = vRAF;
-                    break;
-                }
-            }
-        }
-        if(!window.requestAnimationFrame){
-            manage.console.warning("Control:","RequestAnimationFrame","not supported");
-            window.requestAnimationFrame=function(call){
-                window.setTimeout(call,10);
-            };
-        }
-        //manage.console.debug("RAF:","new RAF selected");
-    },
-    draw:function(){
-        var rat;
-        if(this.needRedraw){
-            rat=manage.manager.draw(this.wantedratio);
-            if(rat===false){
-                this.needRedraw=true;
-                return false;
-            }
-            this.needRedraw=(rat!==this.wantedratio);
-            return rat;
-        }
-        return false;
-        //manage.console.debug("drawing "+this.ratio);
-    },
-    stop:function(){
-        this.running=false;
-        this.wantedratio=this.actratio;
-    },
-    toggle:function(){
-        if(control.settings.play.get()){this.start();}else{this.stop();}
-    },
-    reset:function(){
-        this.set(0);
-        this.setWanted(0);
-        //manage.console.debug("reseted");
-        return 0;
-    },
-    set:function(rat){
-        if(rat===false){return;}
-        if(this.actratio===rat){return;}
-        this.actratio=rat;
-        view.ctrl.slide.byratio(rat);
-        this.needRedraw=true;
-        //manage.console.debug("Control: Actual set to "+rat);
-    },
-    setWanted:function(rat){
-        if(this.wantedratio===rat){return;}
-        this.wantedratio=rat;
-        //view.ctrl.slide.byratio(rat);
-        this.needRedraw=true;
-        //manage.console.debug("Control: Wanted set to "+rat);
-    },
-    notify:function(args){
-        if(args==="toggle"){return this.toggle();}
-        //if(args==="resize"){}
-        this.needRedraw=true;
+ * Copyright (C) 2014  Petr Hošek
+ */
+if (window.control === undefined) {
+  var control = {};
+}
+if (control.control === undefined) {
+  control.control = {};
+}
+$.extend(control.control, {
+  actratio: 0,
+  wantedratio: 0,
+  stats: null,
+  needRedraw: true,
+  running: false,
+  lasttime: 0,
+  fastObser: [],
+  slowObser: [],
+  RAFprefix: null,
+  lastSlow: 1000000,
+  init: function () {
+    var stt = new Stats(),
+        sett = control.settings;
+    stt.setMode(0);
+    $("#all").append(stt.domElement);
+    this.stats = stt;
+    this.findRAF();
+    this.cycle(this.lasttime);
+    sett.zoom.subscribe(this, null);
+    sett.frameposx.subscribe(this, null);
+    sett.frameposy.subscribe(this, null);
+    sett.resol.subscribe(this, null);
+    sett.play.subscribe(this, "toggle");
+    view.axi.subscribe(this, "resize");
+  },
+  start: function () {
+    if (this.actratio >= 1) {
+      this.reset();
     }
+    this.running = true;
+  },
+  cycle: function (stamp) {
+    var dt, nratio, is, lis, rat;
+    if (stamp === undefined) {
+      stamp = new Date().getTime();
+    }
+    dt = stamp - this.lasttime;
+    if (this.running) {
+      nratio = this.wantedratio + dt * control.settings.speed.get() / 10000;
+      if (nratio > 1) {
+        nratio = 1;
+      }
+      if (this.actratio === 1) {
+        if (control.settings.loop.get()) {
+          nratio = this.reset();
+        } else {
+          control.settings.play.set(false);
+        }
+      }
+      this.setWanted(nratio);
+    }
+    //manage.console.debug("Control:","cycle");
+    this.stats.begin();
+    for (is = 0; is < this.fastObser.length; is += 1) {
+      lis = this.fastObser[is];
+      lis.ctx.notify(lis.call);
+    }
+    if (this.lastSlow > 100) {
+      for (is = 0; is < this.slowObser.length; is += 1) {
+        lis = this.slowObser[is];
+        lis.ctx.notify(lis.call);
+      }
+      this.lastSlow = 0;
+    }
+    this.lastSlow += dt;
+    rat = this.draw();
+    this.set(rat);
+    this.stats.end();
+    this.lasttime = stamp;
+    window.requestAnimationFrame(function (stamp) {
+      control.control.cycle(stamp);
+    });
+  },
+  everytick: function (obj, func) {
+    this.fastObser.push({ctx: obj, call: func});
+  },
+  everysec: function (obj, func) {
+    this.slowObser.push({ctx: obj, call: func});
+  },
+  unsubscribe: function (obj, func) {
+    var i, lis;
+    for (i = 0; i < this.fastObser.length; i += 1) {
+      lis = this.fastObser[i];
+      if (lis.ctx === obj && lis.call === func) {
+        this.fastObser.pop(i);
+        return;
+      }
+    }
+    for (i = 0; i < this.slowObser.length; i += 1) {
+      lis = this.slowObser[i];
+      if (lis.ctx === obj && lis.call === func) {
+        this.slowObser.pop(i);
+        return;
+      }
+    }
+    manage.console.warning("Control:", "Unsubscription of", func, "failed");
+  },
+  findRAF: function () {
+    var vendors, i, vRAF;
+    vendors = ['ms', 'moz', 'webkit', 'o'];
+    if (!window.requestAnimationFrame) {
+      for (i = 0; i < vendors.length; i += 1) {
+        vRAF = window[vendors[i] + 'RequestAnimationFrame'];
+        if (vRAF) {
+          window.requestAnimationFrame = vRAF;
+          break;
+        }
+      }
+    }
+    if (!window.requestAnimationFrame) {
+      manage.console.warning("Control:", "RequestAnimationFrame", "not supported");
+      window.requestAnimationFrame = function (call) {
+        window.setTimeout(call, 10);
+      };
+    }
+    //manage.console.debug("RAF:","new RAF selected");
+  },
+  draw: function () {
+    var rat;
+    if (this.needRedraw) {
+      rat = manage.manager.draw(this.wantedratio);
+      if (rat === false) {
+        this.needRedraw = true;
+        return false;
+      }
+      this.needRedraw = (rat !== this.wantedratio);
+      return rat;
+    }
+    return false;
+    //manage.console.debug("drawing "+this.ratio);
+  },
+  stop: function () {
+    this.running = false;
+    this.wantedratio = this.actratio;
+  },
+  toggle: function () {
+    if (control.settings.play.get()) {
+      this.start();
+    } else {
+      this.stop();
+    }
+  },
+  reset: function () {
+    this.set(0);
+    this.setWanted(0);
+    //manage.console.debug("reseted");
+    return 0;
+  },
+  set: function (rat) {
+    if (rat === false) {
+      return;
+    }
+    if (this.actratio === rat) {
+      return;
+    }
+    this.actratio = rat;
+    view.ctrl.slide.byratio(rat);
+    this.needRedraw = true;
+    //manage.console.debug("Control: Actual set to "+rat);
+  },
+  setWanted: function (rat) {
+    if (this.wantedratio === rat) {
+      return;
+    }
+    this.wantedratio = rat;
+    //view.ctrl.slide.byratio(rat);
+    this.needRedraw = true;
+    //manage.console.debug("Control: Wanted set to "+rat);
+  },
+  notify: function (args) {
+    if (args === "toggle") {
+      return this.toggle();
+    }
+    //if(args==="resize"){}
+    this.needRedraw = true;
+  }
 });
 // @license-end
--- public_html/js/manage/manager.js
+++ public_html/js/manage/manager.js
@@ -1,158 +1,172 @@
 /** @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3-or-Later
-* Copyright (C) 2014  Petr Hošek
-*/
-if(window.manage===undefined){var manage={};}
-if(manage.manager===undefined){manage.manager={};}
-$.extend(manage.manager,{
-    lastSpace:null,
-    lastDrawable:null,
-    counter:0,
-    lastTransformed:null,
-    onesecratio:0,
-    init:function(){
-        control.settings.resol.subscribe(this,"resol");
-        control.settings.glwant.subscribe(this,"loaded");
-    },
-    /*draw_text:function(rat){
-        var resol,array,i,j;
-        if(!draw.gl.inited){return false;}
-        resol=control.settings.resol.get();
-        array=new Uint8Array(resol*resol);
-        for(i=0;i<resol;i+=1){
-            for(j=0;j<resol;j+=1){
-                array[j+resol*i]=255*Math.pow(Math.sin(i/resol*Math.PI)*Math.sin((j/resol+rat)*Math.PI),2);
-            }
-        }
-        draw.gl.draw(array);
-        return true;
-    },*/
-    draw:function(rat){
-        var ret,lrat,storaging=false;
-        //if(!draw.gl.inited){return false;}
-        //manage.console.debug("Manager: drawing "+rat);
-        if(!compute.sum_hill.haveData()){return false;}
-        ret=draw.drawer.isInited();
-        if(this.lastSpace===null){
-            ret=this.initSpace()&&ret;
-        }
-        if(!ret){
-            return false;
-        }
-        lrat=this.lastSpace.ratio;
-        //if(rat===0){rat=-1;}
-        if(rat<lrat){
-            this.reset();
-            lrat=-1;
-        }
-        //var nar=this.lastSpace;
-        if(rat!==lrat){
-            if(storaging){
-                manage.storage.load(this.lastSpace,rat);
-            }
-            /*if(isload){
-                manage.console.log("Is loaded at "+this.lastSpace.ratio);
-            }*/
-            //manage.console.debug("Manager: summing "+this.lastSpace.ratio+" to "+rat);
-            if(this.onesecratio===0){
-                this.onesecratio=this.estimate1sec();
-            }
-            if(lrat>=0&&rat>=0&&rat>lrat+this.onesecratio){
-                rat=lrat+this.onesecratio;
-                //manage.console.debug("Manager: ratio reduced to "+rat);
-            }
-            //manage.console.debug("Manager: summing "+this.lastSpace.ratio+" to "+rat);
-            compute.sum_hill.add(this.lastSpace,rat);
-            if(storaging){
-                manage.storage.save(this.lastSpace);
-            }
-            //manage.console.debug("Add from "+this.lastRat+" to "+rat);
-            this.counter+=1;
-            compute.axi.transform(this.lastSpace,this.lastDrawable);
-            this.lastTransformed=null;
-        }
-        draw.drawer.draw(this.lastDrawable,compute.axi.zmax);
-        return rat;
-    },
-    initSpace:function(){
-        //var resol=control.settings.resol.get();
-        this.lastSpace=compute.tspacer.createSpace();
-        if(!this.lastSpace){return false;}
-        this.lastDrawable=this.lastSpace.getDrawable();
-        return true;
-    },
-    setResol:function(){
-        //this.reset();
-        this.lastSpace=null;
-        this.lastDrawable=null;
-        this.lastTransformed=null;
-        manage.storage.reset();
-        compute.axi.lastRatio=-1;
-        this.onesecratio=0;
-    },
-    reset:function(){
-        manage.console.debug("Counter:",this.counter);
-        this.counter=0;
-        if(this.lastSpace){
-            this.lastSpace.reset();
-        }
-        control.control.needRedraw=true;
-    },
-    purge:function(){
-        compute.sum_hill.purge();
-        //this.reset();
-        this.setResol();
-        view.axi.needRedraw=true;
-        compute.axi.reset();
-        manage.storage.reset();
-    },
-    getSpace:function(){
-        return this.lastSpace;
-    },
-    getTransformed:function(){
-        if(this.lastTransformed===null){
-            if(this.lastSpace!==null){
-                this.lastTransformed=compute.axi.transform(this.lastSpace,null,"float32");
-            }
-        }
-        return this.lastTransformed;
-    },
-    dataLoaded:function(){
-        if(!compute.sum_hill.haveData()){return;}
-        this.lastSpace=null;
-        this.lastDrawable=null;
-        control.settings.ncv.set(compute.sum_hill.ncv);
-        this.reset();
-        compute.sum_hill.purgeBlobs();
-        draw.drawer.switchTo();
-    },
-    estimate1sec:function(){
-        var nbody=compute.sum_hill.nbody,
-        resol=control.settings.resol.get();
-        return 1000/nbody*256*256/resol/resol;
-    },
-    refine1sec:function(ratio,time){
-        this.onesecratio=(this.onesecratio+(ratio*1000/time))/2;
-    },
-    notify:function(args){
-        if(args==="resol"){this.setResol();}
-        if(args==="loaded"){this.dataLoaded();}
+ * Copyright (C) 2014  Petr Hošek
+ */
+if (window.manage === undefined) {
+  var manage = {};
+}
+if (manage.manager === undefined) {
+  manage.manager = {};
+}
+$.extend(manage.manager, {
+  lastSpace: null,
+  lastDrawable: null,
+  counter: 0,
+  lastTransformed: null,
+  onesecratio: 0,
+  init: function () {
+    control.settings.resol.subscribe(this, "resol");
+    control.settings.glwant.subscribe(this, "loaded");
+  },
+  /*draw_text:function(rat){
+   var resol,array,i,j;
+   if(!draw.gl.inited){return false;}
+   resol=control.settings.resol.get();
+   array=new Uint8Array(resol*resol);
+   for(i=0;i<resol;i+=1){
+   for(j=0;j<resol;j+=1){
+   array[j+resol*i]=255*Math.pow(Math.sin(i/resol*Math.PI)*Math.sin((j/resol+rat)*Math.PI),2);
+   }
+   }
+   draw.gl.draw(array);
+   return true;
+   },*/
+  draw: function (rat) {
+    var ret, lrat, storaging = false;
+    //if(!draw.gl.inited){return false;}
+    //manage.console.debug("Manager: drawing "+rat);
+    if (!compute.sum_hill.haveData()) {
+      return false;
     }
+    ret = draw.drawer.isInited();
+    if (this.lastSpace === null) {
+      ret = this.initSpace() && ret;
+    }
+    if (!ret) {
+      return false;
+    }
+    lrat = this.lastSpace.ratio;
+    //if(rat===0){rat=-1;}
+    if (rat < lrat) {
+      this.reset();
+      lrat = -1;
+    }
+    //var nar=this.lastSpace;
+    if (rat !== lrat) {
+      if (storaging) {
+        manage.storage.load(this.lastSpace, rat);
+      }
+      /*if(isload){
+       manage.console.log("Is loaded at "+this.lastSpace.ratio);
+       }*/
+      //manage.console.debug("Manager: summing "+this.lastSpace.ratio+" to "+rat);
+      if (this.onesecratio === 0) {
+        this.onesecratio = this.estimate1sec();
+      }
+      if (lrat >= 0 && rat >= 0 && rat > lrat + this.onesecratio) {
+        rat = lrat + this.onesecratio;
+        //manage.console.debug("Manager: ratio reduced to "+rat);
+      }
+      //manage.console.debug("Manager: summing "+this.lastSpace.ratio+" to "+rat);
+      compute.sum_hill.add(this.lastSpace, rat);
+      if (storaging) {
+        manage.storage.save(this.lastSpace);
+      }
+      //manage.console.debug("Add from "+this.lastRat+" to "+rat);
+      this.counter += 1;
+      compute.axi.transform(this.lastSpace, this.lastDrawable);
+      this.lastTransformed = null;
+    }
+    draw.drawer.draw(this.lastDrawable, compute.axi.zmax);
+    return rat;
+  },
+  initSpace: function () {
+    //var resol=control.settings.resol.get();
+    this.lastSpace = compute.tspacer.createSpace();
+    if (!this.lastSpace) {
+      return false;
+    }
+    this.lastDrawable = this.lastSpace.getDrawable();
+    return true;
+  },
+  setResol: function () {
+    //this.reset();
+    this.lastSpace = null;
+    this.lastDrawable = null;
+    this.lastTransformed = null;
+    manage.storage.reset();
+    compute.axi.lastRatio = -1;
+    this.onesecratio = 0;
+  },
+  reset: function () {
+    manage.console.debug("Counter:", this.counter);
+    this.counter = 0;
+    if (this.lastSpace) {
+      this.lastSpace.reset();
+    }
+    control.control.needRedraw = true;
+  },
+  purge: function () {
+    compute.sum_hill.purge();
+    //this.reset();
+    this.setResol();
+    view.axi.needRedraw = true;
+    compute.axi.reset();
+    manage.storage.reset();
+  },
+  getSpace: function () {
+    return this.lastSpace;
+  },
+  getTransformed: function () {
+    if (this.lastTransformed === null) {
+      if (this.lastSpace !== null) {
+        this.lastTransformed = compute.axi.transform(this.lastSpace, null, "float32");
+      }
+    }
+    return this.lastTransformed;
+  },
+  dataLoaded: function () {
+    if (!compute.sum_hill.haveData()) {
+      return;
+    }
+    this.lastSpace = null;
+    this.lastDrawable = null;
+    control.settings.ncv.set(compute.sum_hill.ncv);
+    this.reset();
+    compute.sum_hill.purgeBlobs();
+    draw.drawer.switchTo();
+  },
+  estimate1sec: function () {
+    var nbody = compute.sum_hill.nbody,
+        resol = control.settings.resol.get();
+    return 1000 / nbody * 256 * 256 / resol / resol;
+  },
+  refine1sec: function (ratio, time) {
+    this.onesecratio = (this.onesecratio + (ratio * 1000 / time)) / 2;
+  },
+  notify: function (args) {
+    if (args === "resol") {
+      this.setResol();
+    }
+    if (args === "loaded") {
+      this.dataLoaded();
+    }
+  }
 });
 // ############################################################xx
 // Allrunner
-$(function(){
-    control.settings.onload();  
-    control.control.init();
-    view.ctrl.init();
-    view.axi.init();
-    control.gestures.init();
-    compute.reader.init();
-    manage.manager.init();
-    control.measure.onload();
-    view.exporter.onload();
-    manage.tests.onload();
-    control.settings.init();
-    view.panel.init();
-    draw.drawer.onload();
-}); 
+$(function () {
+  control.settings.onload();
+  control.control.init();
+  view.ctrl.init();
+  view.axi.init();
+  control.gestures.init();
+  compute.reader.init();
+  manage.manager.init();
+  control.measure.onload();
+  view.exporter.onload();
+  manage.tests.onload();
+  control.settings.init();
+  view.panel.init();
+  draw.drawer.onload();
+});
 // @license-end
